9a18beb1fee4513ae8bd5851b324442d74ef59e1
Hugo Pereira <hugopereira.contato@gmail.com>
2024-06-10 04:07:52 -0300

    chore: add register account

--- scripts/home.js
+++ scripts/home.js
@@ -1,14 +1,63 @@
+// REGISTER FORM
 
- var swiper = new Swiper(".swiper", {
-   speed: 1000,
-   loop: true,
-   pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-   },
-   keyboard: true,
-   // autoplay: {
-   //    delay: 3500,
-   //    disableOnInteraction: false,
-   // }
- })
+ function togglePassword(id, icon) {
+   const input = document.getElementById(id);
+   if (input.type === "password") {
+       input.type = "text";
+       icon.textContent = 'visibility';
+   } else {
+       input.type = "password";
+       icon.textContent = 'visibility_off';
+   }
+}
+
+// REGISTER FORM
+
+// Register Form
+
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+
+form.addEventListener("submit", (event) => {
+   event.preventDefault();
+
+   checkInputName();
+   checkInputEmail();
+})
+
+function checkInputName() {
+   const usernameValue = username.value;
+
+   if (usernameValue === "") {
+      errorInput(username, "Preencha seu nome!")
+   }else {
+      const formItem = username.parentElement;
+      formItem.className = "form-content"
+   }
+}
+
+function checkInputEmail() {
+   const emailValue = email.value;
+
+   if(emailValue === "") {
+      errorInput(email, "O email é obrigatório.")
+   }else {
+      const formItem = email.parentElement;
+      formItem.className = "form-content"
+   }
+}
+
+
+function errorInput(input, message) {
+   const formItem = input.parentElement;
+   const textMessage = formItem.querySelector(".error-message")
+
+   textMessage.innerText = message;
+
+   formItem.className = "form-content error"
+}
+
+
---
+++ scripts/swiper.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".swiper", {
+   speed: 1000,
+   loop: true,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
+   keyboard: true,
+   // autoplay: {
+   //    delay: 3500,
+   //    disableOnInteraction: false,
+   // }
+ })
